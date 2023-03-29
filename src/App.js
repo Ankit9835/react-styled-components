56b5d30ff5682f9ad1fbdf9ffa8442ebd2a232f5
Ankit9835 <ankydchrismatic@gmail.com>
2023-03-29 18:56:30 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import styled from 'styled-components'
 import BasicTitle from './components/BasicTitle'
+import Card from './components/Card'
 import ComplexTitle from './components/ComplexTitle'
 import {DefaultButton, HipsterComponent} from './components/DefaultButton'
+import GlobalStyles from './global-styles'
+import { ThemeProvider } from 'styled-components'
+import { useState } from 'react'
+import Loading from './components/Loading'
+
+
+const BaseTheme = {
+  color:'#222',
+  background:'#fff'
+}
+
+const DarkTheme = {
+  color:'#fff',
+  background:'#222'
+}
+
+const Conteiner = styled.div`
+  padding:2rem;
+  color:${(props) => props.theme}
+`
 
 function App() {
+  const [theme,setTheme] = useState(true)
+  const toggleMe = () => {
+    return setTheme(!theme)
+  }
   return (
     <>
-       <BasicTitle>styled components</BasicTitle>
+       {/* <BasicTitle>styled components</BasicTitle>
        <BasicTitle special>styled components</BasicTitle>
        <DefaultButton>click me</DefaultButton>
        <HipsterComponent>click me</HipsterComponent>
-       <ComplexTitle title='Complex Title'></ComplexTitle>
+       <ComplexTitle title='Complex Title'></ComplexTitle> */}
+       <Loading />
+       {/* <ThemeProvider theme={theme ? BaseTheme : DarkTheme}>
+       <GlobalStyles />
+          <Card />
+          <Conteiner>
+            <h1>hello react</h1>
+            <button className='btn' onClick={toggleMe}>toggle me</button>
+          </Conteiner>
+       </ThemeProvider> */}
     </>
   )
 }
--- src/components/ComplexTitle.js
+++ src/components/ComplexTitle.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
+import { colors, setUpBorder } from '../utils'
+
 
 const ComplexTitle = ({title}) => {
   return (
     <Wrapper>
-        <h1>{title}</h1>
+        <h1 className='text'>{title}</h1>
         <div className='underline'></div>
+        <div className='box'></div>
     </Wrapper>
   )
 }
@@ -14,12 +17,20 @@ const Wrapper = styled.div`
     h1{
         text-transform:capitalize;
         text-align:center;
+        color:yellow;
     }
     .underline{
         width:5rem;
         height:0.25rem;
-        background:#645cff;
+        background: ${colors.primary};
         margin:0 auto;
+    }
+    .text{
+        color:red
+    }
+    .box{
+        height:10px;
+        border:${setUpBorder({width:5,type:'solid',color:'red'})}
     }
 `
 
